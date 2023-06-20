1a2d23047ffc5cc61ffb8257a428a02e9d488490
Chandara-Sin <cdsin68@gmail.com>
2023-06-20 23:46:19 +0700

    refactor(ChatInput): add handle onchange event

--- components/Message/ChatInput.tsx
+++ components/Message/ChatInput.tsx
@@ -1,18 +1,44 @@
-import { PaperPlan } from "@/svg/Icons";
-import React from "react";
+"use client";
 
-const ChatInput = () => (
-  <footer className="fixed bottom-0 w-screen z-50 bg-white rounded-t-3xl p-6 shadow-sm">
-    <section className="flex items-center bg-gray-100 rounded-xl border-2 border-gray-100 focus:border-b-2 outline-0">
-      <input
-        className="h-12 w-full bg-transparent border-none appearance-none p-2 text-gray-600 focus:outline-none"
-        type="text"
-        placeholder="Type here..."
-      />
-      <hr className="flex-shrink-0 w-[1px] h-8 bg-gray-400 text-transparent" />
-      <PaperPlan className="flex-shrink-0 text-blue-400 hover:text-blue-600 cursor-pointer mx-4" />
-    </section>
-  </footer>
-);
+import { PaperPlan } from "@/svg/Icons";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+const ChatInput = () => {
+  const [message, setMessage] = useState("");
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setMessage(value);
+  };
+  const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setMessage("");
+  };
+  return (
+    <footer className="fixed bottom-0 w-screen z-50 bg-white rounded-t-3xl p-6 shadow-sm">
+      <form
+        onSubmit={handleOnSubmit}
+        className="flex items-center bg-gray-100 rounded-xl border-2 border-gray-100 focus:border-b-2 outline-0"
+      >
+        <input
+          className="h-12 w-full bg-transparent border-none appearance-none p-2 text-gray-600 focus:outline-none"
+          type="text"
+          placeholder="Type here..."
+          value={message}
+          onChange={handleOnChange}
+        />
+        <hr className="flex-shrink-0 w-[1px] h-8 bg-gray-400 text-transparent" />
+        <button className="flex-shrink-0 mx-4" type="submit">
+          <PaperPlan
+            className={`${
+              message
+                ? "text-blue-500 hover:text-blue-700 cursor-pointer"
+                : "text-blue-200 cursor-auto"
+            }`}
+          />
+        </button>
+      </form>
+    </footer>
+  );
+};
 
 export default ChatInput;
