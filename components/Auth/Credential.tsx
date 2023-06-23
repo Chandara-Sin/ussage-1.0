eb2f9f3e058b80deb0a29da52eaf159f5dcf2b5d
Chandara-Sin <>
2023-06-23 14:18:22 +0700

    init google and credential login

---
+++ components/Auth/Credential.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import { FormEvent } from "react";
+
+const CredentialSignIn = () => {
+  const baseURL = process.env.VERCEL_URL ?? "http://localhost:3000";
+  const handleOnSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    await signIn("credentials", {
+      email: formData.get("email"),
+      callbackUrl: baseURL,
+    });
+  };
+  return (
+    <form className="w-full max-w-md" onSubmit={handleOnSubmit}>
+      <h1 className="mt-3 text-2xl font-semibold text-gray-800 capitalize font-mono">
+        sign In
+      </h1>
+      <div className="relative flex items-center mt-8">
+        <input
+          type="email"
+          className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-4 focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+          placeholder="Enter your email"
+          name="email"
+        />
+      </div>
+      <button
+        className="w-full mt-5 px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50 font-mono"
+        type="submit"
+      >
+        Sign in
+      </button>
+      <div className="relative flex items-center mt-5">
+        <span className="flex-grow border-t border-gray-400"></span>
+        <span className="flex-shrink mx-4 text-gray-400">OR</span>
+        <span className="flex-grow border-t border-gray-400"></span>
+      </div>
+    </form>
+  );
+};
+
+export default CredentialSignIn;
---
+++ components/Auth/Google.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { GoogleLogo } from "@/svg/Icons";
+import { getProviders, signIn } from "next-auth/react";
+
+const Google = ({
+  providers,
+}: {
+  providers: Awaited<ReturnType<typeof getProviders>>;
+}) => {
+  const baseURL = process.env.VERCEL_URL ?? "http://localhost:3000";
+  return (
+    <button
+      className="w-full max-w-md flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-200"
+      onClick={() =>
+        signIn(providers?.google.id, {
+          callbackUrl: baseURL,
+          basePath: process.env.NEXTAUTH_URL ?? baseURL,
+        })
+      }
+    >
+      <GoogleLogo width={25} height={25} />
+      <span className="mx-2 text-gray-700">
+        Sign in with {providers?.google.name}
+      </span>
+    </button>
+  );
+};
+
+export default Google;
