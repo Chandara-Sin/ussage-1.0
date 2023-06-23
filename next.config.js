4aef40550a56f914d66855d5c24f7a22181b6e15
Chandara-Sin <cdsin68@gmail.com>
2023-06-23 14:07:32 +0700

    config next img

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { domains: ["avatars.githubusercontent.com"] } };
+const nextConfig = {
+  images: {
+    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
+};
 
 module.exports = nextConfig;
