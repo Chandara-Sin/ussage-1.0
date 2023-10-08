7bfc6d8a3d2d4aa107ce97f2d69eab52cb250743
Chandara Sin <cdsin68@gmail.com>
2023-10-08 22:40:20 +0700

    feat: add opengraph to enhance seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,25 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Ussage_1.0",
+export const metadata: Metadata = {
+  title: "Ussage",
   description: "Ussage Chat App",
+  openGraph: {
+    type: "website",
+    url: "ussage-1-0.vercel.app",
+    title: "Ussage - Your Ultimate Messaging Companion",
+    description:
+      "Ussage: Instant messaging, voice/video calls, privacy, and more.",
+    siteName: "Ussage",
+    images: [
+      {
+        url: "ussage-1-0.vercel.app",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
