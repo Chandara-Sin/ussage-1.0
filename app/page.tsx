7c74d95ae3d8f838d2fab3ffeed0c777f456cbec
Chandara Sin <cdsin68@gmail.com>
2023-10-15 22:35:23 +0700

    fix: server side rendering on load

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ const baseURL = process.env.VERCEL_URL ?? "http://localhost:3000";
 
 const Home = async () => {
   const session = await checkAuth();
-  const { messages } = await fetch(`${baseURL}/api/messages`, {
-    cache: "reload",
-  }).then((res) => res.json());
+  const { messages } = await fetch(`${baseURL}/api/messages`).then((res) =>
+    res.json()
+  );
   return (
     <main className="min-h-screen bg-slate-100">
       <Header session={session} />
